66380c8f240aea194d2d3e9d70f55c247b0b6862
Afauvre <43547930+Afauvre@users.noreply.github.com>
2020-06-23 21:44:29 -0700

    Community Node
    Added community node program to TOC

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -42,6 +42,7 @@ module.exports = {
           ['/operators/architecture-overview', 'Network Architecture Overview'],
           ['/operators/prerequisites', 'Prerequisites Guide'],
           ['/operators/amber-network', 'Amber Network'],
+          ['/operators/amber/communitynode.md', 'Community Node Program'],
           [
             '/operators/hardware-recommendations',
             'Node Hardware Recommendations',
